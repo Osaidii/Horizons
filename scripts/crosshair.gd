b6e2b5ec19f8f0d3ee6c1717334a9d0798fe00cb
Osaidi1 <hassanosaid123@gmail.com>
2026-04-08 15:56:21 +0200

    small level design

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -3,4 +3,4 @@ extends Control
 func _draw() -> void:
 	# Draw Circle
 	var center := get_viewport_rect().end / 2
-	draw_circle(center, 1, Color.WHITE, true, -1, true)
+	draw_circle(center, 2, Color.WHITE, true, -1, true)
