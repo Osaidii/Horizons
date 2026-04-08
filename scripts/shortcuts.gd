b36e2719a69117ddff7289ccec1c4a7d8481ff8c
Osaidi1 <hassanosaid123@gmail.com>
2026-04-08 14:29:47 +0200

    simple scene setup, intro done, work on domain 1 now

--- scripts/shortcuts.gd
+++ scripts/shortcuts.gd
@@ -3,6 +3,12 @@ extends Node
 var current_resolution := Vector2(1920, 1080)
 var design_resolution := Vector2(1280, 720)
 
+# Levels
+var intro_complete: bool
+var domain_1_complete: bool
+var domain_2_complete: bool
+var domain_3_complete: bool
+
 # Shortcut using Variables (Signal Substitute)
 var increase_flashlight_battery: int
 var flash_light_unlocked: bool
